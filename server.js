5efaf86a5b1df03846e4e1c82d3df6ede29143f9
Aditya Kumar Sah <141633697+Aditya-kumar-sah@users.noreply.github.com>
2024-06-19 21:05:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname,'./client/build')))
 app.get('*',function(req,res){
        res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 //LISTEN
 app.listen(PORT,()=>{
