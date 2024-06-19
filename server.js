b9e6eb8a01b663543cffea2fe98f5b9f8d9110b2
Aditya Kumar Sah <141633697+Aditya-kumar-sah@users.noreply.github.com>
2024-06-19 19:32:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const connectDb = require('./config/connectDb')
+const useRoute = require('./routes/userRoutes')
+const path = requie('path')
+
+//rest object
+const app = express()
+
+dotenv.config({path : './config.env'})
+
+
+//databaseCall
+connectDb();
+//middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+//user routes
+app.use('/api/v1/users',useRoute)
+
+//transaction routes
+app.use('/api/v1/transaction',require("./routes/transactionRoutes"))
+
+//for hosting
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+app.get('*',function(req,res){
+       res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+const PORT = process.env.PORT
+
+//LISTEN
+app.listen(PORT,()=>{
+    console.log(`Server is live on port ${PORT}`);
+})
